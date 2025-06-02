0a64effb753e75156e3d3b8a1043d3db6c7d6607
Matheus-Secundario <matheus.pires218321@gmail.com>
2025-06-01 22:42:52 -0300

    Add files via upload

---
+++ Monitoramento/Trabalho/Monitoramento/Program.cs
@@ -0,0 +1,19 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar;
+using Google.Apis.Calendar.v3; ////////////////////
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Drive.v3.Data; ////////////////
+using System.Globalization;
+
+class Program
+{
+    CalendarService Calendario = new CalendarService(); //Eu acho que uma classe para calendário
+    UserCredential credencial = new UserCredential(); 
+
+    public Program()
+    {
+        
+    }
+
+
+}
